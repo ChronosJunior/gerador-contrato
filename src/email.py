b6b593a9dc9153d73a9a516f9ddf810d7f607dbc
caian23 <caian23@outlook.com>
2025-12-22 16:24:19 -0300

    fix: add dotenv to read env vars

--- src/email.py
+++ src/email.py
@@ -1,5 +1,6 @@
 import smtplib
 import os
+from dotenv import load_dotenv 
 
 def connect_smtp_server(smtp_server, port, sender_email, sender_pass):
     try:
@@ -11,6 +12,7 @@ def connect_smtp_server(smtp_server, port, sender_email, sender_pass):
     return mail_server
 
 def send_email(addressee, message):
+    load_dotenv()
     config = {
         "smtp_server": os.getenv("SMTP_SERVER"),
         "port": int(os.getenv("SMTP_PORT", 587)),
