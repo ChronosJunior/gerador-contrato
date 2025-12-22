1a2ec4ac5dfe3f32263aebde90946ce0b6a486ad
caian23 <caian23@outlook.com>
2025-12-22 16:48:51 -0300

    refactor: changed exception treatment

--- src/email.py
+++ src/email.py
@@ -7,23 +7,25 @@ def connect_smtp_server(smtp_server, port, sender_email, sender_pass):
         mail_server = smtplib.SMTP(smtp_server, port)
         mail_server.starttls()
         mail_server.login(sender_email, sender_pass)
-    except Exception:
-        raise Exception
+    except Exception as e:
+        print(f"{e}")
+        raise
     return mail_server
 
-def send_email(addressee, message):
-    load_dotenv()
-    config = {
-        "smtp_server": os.getenv("SMTP_SERVER"),
-        "port": int(os.getenv("SMTP_PORT", 587)),
-        "sender_email": os.getenv("SENDER_EMAIL"),
-        "sender_pass": os.getenv("SENDER_PASS")
-    }
-    
+def send_email(addressee, message, server=False):
     try:
-        server = connect_smtp_server(**config)
+        if not server:
+            load_dotenv()
+            config = {
+                "smtp_server": os.getenv("SMTP_SERVER"),
+                "port": int(os.getenv("SMTP_PORT", 587)),
+                "sender_email": os.getenv("SENDER_EMAIL"),
+                "sender_pass": os.getenv("SENDER_PASS")
+            }
+            server = connect_smtp_server(**config)
         server.sendmail(os.getenv("SENDER_EMAIL"), addressee, message)
         server.quit()
         return True 
-    except Exception:
-        return False
+    except Exception as e:
+        print(f"{e}")
+        raise
